b2f00cca60d05e6cc13a69cb2ac58e77b9d53194
sejung2 <129058043+sejung2@users.noreply.github.com>
2024-09-23 23:03:18 +0900

    [level 1] Title: 예산, Time: 0.16 ms, Memory: 32.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,10 @@
+function solution(d, budget) {
+    let count = 0;
+    d.sort((a,b)=>a-b);
+    for(let i = 0; i<d.length; i++){
+        budget -= d[i];
+        if(budget<0)break;
+        count++;
+    }
+    return count;
+}
