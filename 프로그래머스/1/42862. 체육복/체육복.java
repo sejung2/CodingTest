7b861fb86cdcb14847aa4190e55e12afb85e9da7
seing <129058043+sejung2@users.noreply.github.com>
2024-11-29 11:38:41 +0900

    Update 체육복.java

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
         int count = 0;
         int[] student = new int[n];
         
@@ -16,23 +14,14 @@ class Solution {
         }
         
         for(int i = 0; i < lost.length; i++){
-            if(student[lost[i]-1] == 2){
-                student[lost[i]-1] = 1;
-                continue;
-            } else student[lost[i]-1] = 0;
-            
-        }
-        
-        for(int i : student){
-            System.out.println("test :" + i);
+            student[lost[i]-1]--;
         }
         
         for(int i = 0; i < n-1; i++){
             if(student[i] == 0 && student[i+1] == 2){
                 student[i+1]--;
                 student[i]++;
-            }
-            if(student[i] == 2 && student[i+1] == 0){
+            } else if(student[i] == 2 && student[i+1] == 0){
                 student[i+1]++;
                 student[i]--;
             }
@@ -41,7 +30,6 @@ class Solution {
         
         for(int i = 0; i < n; i++){
             if(student[i] > 0){
-                System.out.println(i + " check : " + student[i]);
                 count++;
             }
         }
