3c7653a772e434826d20b2c2a237e6135df2ca15
seing <a91732167@gmail.com>
2025-06-28 15:33:37 +0900

    [level 1] Title: 시저 암호, Time: 1.98 ms, Memory: 86.4 MB -BaekjoonHub

--- Java/프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ Java/프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -6,16 +6,22 @@ class Solution {
         char[] target = s.toCharArray();
         
         for(int i = 0; i < target.length; i++){
-            char input = (char)(target[i] +n);
-            if(target[i] > 64 && target[i] < 91){
-                if(input > 90){
-                    word.append((char)(input - 26));
-                } else word.append(input);
-            } else if(target[i] > 96 && target[i] < 123){
-                if(input > 122){
-                    word.append((char)(input - 26));
-                } else word.append(input);
-            } else word.append(target[i]);
+            if(target[i] == ' '){
+                word.append(target[i]);
+                continue;
+            }
+            
+            if(Character.isUpperCase(target[i])){
+                char base = 'A';
+                int input = target[i] - base;
+                int dist = (input + n) % 26;
+                word.append((char)(dist + base));
+            } else {
+                char base = 'a';
+                int input = target[i] - base;
+                int dist = (input + n) % 26;
+                word.append((char)(dist + base));
+            }
         }
         
         return word.toString();
