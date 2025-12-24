ec1d1536ebfdadd7a2f8f48014004be95b0ed7b7
seing <seingDev@gmail.com>
2025-12-24 10:37:26 +0900

    Time: 0 ms (100%), Space: 45.3 MB (8.84%) - LeetHub

--- LeetCode/Easy/0112-path-sum/0112-path-sum.java
+++ LeetCode/Easy/0112-path-sum/0112-path-sum.java
@@ -2,23 +2,7 @@ class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
         if((root.val == targetSum) && (root.left == null && root.right == null)) return true;
-        boolean left = sum(root.left, targetSum - root.val);
-        boolean right = sum(root.right, targetSum - root.val);
-        return left || right;
-    }
-
-    public boolean sum(TreeNode root, int targetSum) {
-        if(root == null) return false;
-        int target = targetSum - root.val;
-        if(target == 0){
-            if(root.left == null && root.right == null){
-                return true;
-            }
-        }
-
-        boolean left = sum(root.left, target);
-        boolean right = sum(root.right, target);
-
-        return left || right;
+        
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
