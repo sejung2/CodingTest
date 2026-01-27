f71a3db9aaa0d3832c972e18dabf746f1ca53bdc
seing <seingDev@gmail.com>
2026-01-27 11:39:42 +0900

    Time: 1 ms (38.44%), Space: 44.9 MB (68.99%) - LeetHub

---
+++ LeetCode/Easy/0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int[] memo = new int[cost.length];
+        memo[0] = cost[0];
+        memo[1] = cost[1];
+
+        for (int i = 2; i < cost.length; i++) {
+            memo[i] = Math.min(memo[i - 1], memo[i - 2]) + cost[i];
+        }
+
+        return Math.min(memo[memo.length - 1], memo[memo.length - 2]);
+    }
+}
