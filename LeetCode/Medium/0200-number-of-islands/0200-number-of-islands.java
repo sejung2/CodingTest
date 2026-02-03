b0188ad44ab11d31c8bfa5e9c7b4f22639c1f98c
seing <seingDev@gmail.com>
2026-02-03 10:25:45 +0900

    Time: 3 ms (87.54%), Space: 52.6 MB (20.34%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -1,18 +1,18 @@
 class Solution {
 
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    int N,M;
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+    int N, M;
 
     public int numIslands(char[][] grid) {
         N = grid.length;
         M = grid[0].length;
         int result = 0;
 
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 if (grid[i][j] == '1') {
-                    bfs(grid, i, j);
+                    dfs(grid, i, j);
                     result++;
                 }
             }
@@ -20,27 +20,18 @@ class Solution {
         return result;
     }
 
-    void bfs(char[][] grid, int x, int y) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y});
-        grid[x][y] = '2';
+    void dfs(char[][] grid, int x, int y) {
+        if (x < 0 || y < 0 || x >= N || y >= M || grid[x][y] != '1') {
+            return;
+        }
 
-        while (!q.isEmpty()) {
-            int[] current = q.poll();
-            int cx = current[0];
-            int cy = current[1];
+        grid[x][y] = '0';
 
-            for (int i = 0; i < 4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M || grid[nx][ny] == '0' || grid[nx][ny] == '2') {
-                    continue;
-                }
-
-                q.add(new int[]{nx, ny});
-                grid[nx][ny] = '2';
-            }
+            dfs(grid, nx, ny);
         }
     }
 }
