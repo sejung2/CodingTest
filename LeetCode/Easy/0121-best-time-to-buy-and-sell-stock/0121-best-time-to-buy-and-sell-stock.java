de0500b3017fddfa7d39d9125ff48d78bdbf3921
seing <seingDev@gmail.com>
2026-02-25 11:54:49 +0900

    Time: 1 ms (99.73%), Space: 94.3 MB (85.8%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        int minPrice = prices[0];
+        int profit = 0;
+        int min = prices[0];
 
         for(int i = 0; i < prices.length; i++){
-            if(minPrice > prices[i]) minPrice = prices[i];
-            if(maxProfit < prices[i] - minPrice) maxProfit = prices[i] - minPrice;
+            min = Math.min(min, prices[i]);
+            profit = Math.max(profit, prices[i] - min);
         }
         
-        return maxProfit;
+        return profit;
     }
 }
