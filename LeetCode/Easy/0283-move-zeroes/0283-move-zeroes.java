46f34dafdde153355f4ed8a702e49cb1377cf992
seing <seingDev@gmail.com>
2026-02-26 10:43:59 +0900

    Time: 2 ms (90.21%), Space: 47.8 MB (56.93%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int numPos = 0;
-
+        int input = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                int temp = nums[i];
-                nums[numPos] = nums[i];
-                nums[i] = temp;
-                numPos++;
-            }
+            if (nums[i] == 0) continue;
+            int temp = nums[i];
+            nums[i] = nums[input];
+            nums[input] = temp;
+
+            input++;
         }
     }
 }
