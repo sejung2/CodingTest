6b3093970feecfef30686a2e572e39c4c578ff5a
seing <seingDev@gmail.com>
2026-03-04 11:34:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.63 ms, Memory: 97 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        
+        for (String c : completion) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) == 1) {
+                return key;
+            }
+        }
+        return "";
+    }
+}
