483eb755a60c47f8b66b90b42df1628a283758b3
seing <seingDev@gmail.com>
2026-03-05 10:46:01 +0900

    [level 1] Title: 폰켓몬, Time: 5.61 ms, Memory: 80.8 MB -BaekjoonHub

--- Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         Set<Integer> set = new HashSet<>();
-        for(int i = 0; i < nums.length; i++){
-            set.add(nums[i]);
+        for (int i : nums) {
+            set.add(i);
         }
         int limit = nums.length / 2;
         return set.size() > limit ? limit : set.size();
